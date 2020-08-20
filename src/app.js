547f7f30dc252d133f65a7655951ddf6d785fc41
Ane Mendes <ralianemc@gmail.com>
2020-08-20 00:19:48 -0300

    GoStack Rocketseat Nodejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,54 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function logRequests(request, response, next){
+  const { method, url} = request;
+  const logLabel = `[${method.toUpperCase()}]`;
+
+  console.log(logLabel);
+
+  app.use(logRequests);
+
+  return next
+}
+
+app.get('/repositories', (request, response) => {
+    response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const { title, url,techs } = request.body;
+
+    const repository = { id: uuid(), title:title, url:url, techs:techs, likes:likes };
+
+    repositories.push(repository);
+    return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+    const { id } = request.params;
+
+    const { title, url, techs } = request.body;
+
+    });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const { id } = request.params;
+
+    const repository = repositories.find(repository => repository.id === id);
+
+    repository.likes += 1;
+
+    return response.json(repository);
 });
 
 module.exports = app;
+
+const newLocal = 3333;
+app.listen(newLocal, () => {
+  console.log('🎈 Back-end started');
+});
